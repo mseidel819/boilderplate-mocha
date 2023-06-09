c3c2226cf389c6918c9ee62924fc2a8c765b1442
Matt Seidel <mseidel819@gmail.com>
2023-06-09 09:33:57 -0500

    5,6

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -42,17 +42,21 @@ suite("Unit Tests", function () {
   suite("Equality", function () {
     // #5
     test("#equal, #notEqual", function () {
-      assert.fail(12, "12", "Numbers are coerced into strings with ==");
-      assert.fail({ value: 1 }, { value: 1 }, "== compares object references");
-      assert.fail(6 * "2", "12");
-      assert.fail(6 + "2", "12");
+      assert.equal(12, "12", "Numbers are coerced into strings with ==");
+      assert.notEqual(
+        { value: 1 },
+        { value: 1 },
+        "== compares object references"
+      );
+      assert.equal(6 * "2", "12");
+      assert.notEqual(6 + "2", "12");
     });
     // #6
     test("#strictEqual, #notStrictEqual", function () {
-      assert.fail(6, "6");
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * "2", 12);
-      assert.fail([1, "a", {}], [1, "a", {}]);
+      assert.notStrictEqual(6, "6");
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * "2", 12);
+      assert.notStrictEqual([1, "a", {}], [1, "a", {}]);
     });
     // #7
     test("#deepEqual, #notDeepEqual", function () {
